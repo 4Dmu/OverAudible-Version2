8fe015b47c667489edbf4674ffe5abfcebfdb8f4
mu <muspam922@gmail.com>
2022-07-07 13:18:33 +0100

    Created data service

---
+++ OverAudible/OverAudible/Services/DataService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using OverAudible.DbContexts;
+using OverAudible.Models;
+using OverAudible.Models.DTOs;
+
+namespace OverAudible.Services
+{
+    public class DataService : IDataService<Item>
+    {
+        private readonly MainDbContextFactory _factory;
+
+        public DataService(MainDbContextFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public async Task<Item> Create(Item entity)
+        {
+            using (MainDbContext context = _factory.CreateDbContext())
+            {
+                var dto = new ItemDTO() 
+                { 
+                    Asin = entity.Asin, 
+                    Item = JsonConvert.SerializeObject(entity) 
+                };
+
+                await context.OfflineLibrary.AddAsync(dto);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            using (MainDbContext context = _factory.CreateDbContext())
+            {
+                ItemDTO? dto = await context.OfflineLibrary.FirstOrDefaultAsync(x =>
+                    x.Asin == id);
+                if (dto == null)
+                    return false;
+
+                context.OfflineLibrary.Remove(dto);
+                await context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public async Task<bool> DeleteAll()
+        {
+            using (MainDbContext context = _factory.CreateDbContext())
+            {
+                context.OfflineLibrary.RemoveRange(context.OfflineLibrary.ToList());
+                await context.SaveChangesAsync();
+                return true;
+            }
+        }
+
+        public async Task<List<Item>> GetAll()
+        {
+            using (MainDbContext context = _factory.CreateDbContext())
+            {
+                var items = await context.OfflineLibrary.ToListAsync();
+
+                List<Item> result = new List<Item>();
+
+                foreach (var item in items)
+                {
+                    result.Add(JsonConvert.DeserializeObject<Item>(item.Item));
+                }
+
+                return result;
+            }
+        }
+
+        public async Task<Item> GetById(string id)
+        {
+            using (MainDbContext context = _factory.CreateDbContext())
+            {
+                ItemDTO dto = await context.OfflineLibrary.FirstOrDefaultAsync(x => x.Asin == id);
+                return JsonConvert.DeserializeObject<Item>(dto.Item);
+            }
+        }
+
+        public async Task<Item> Update(string id, Item entity)
+        {
+            using (MainDbContext context = _factory.CreateDbContext())
+            {
+                entity.Asin = id;
+
+                var dto = new ItemDTO()
+                {
+                    Asin = entity.Asin,
+                    Item = JsonConvert.SerializeObject(entity)
+                };
+
+                context.OfflineLibrary.Update(dto);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+    }
+}
---
+++ OverAudible/OverAudible/Services/IDataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverAudible.Services
+{
+    public interface IDataService<T>
+    {
+        Task<List<T>> GetAll();
+
+        Task<T> GetById(string id);
+
+        Task<T> Create(T entity);
+
+        Task<T> Update(string id,T entity);
+
+        Task<bool> Delete(string id);
+
+        Task<bool> DeleteAll();
+    }
+
+    
+}
